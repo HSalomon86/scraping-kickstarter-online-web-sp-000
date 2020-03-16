d8f0b025a7d6702752682cf1c8ec811907b4a35e
Hendrick Salomon <hsalomon86@gmail.com>
2020-03-16 13:49:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,7 +21,7 @@ def create_project_hash
   end
 
   # return the projects hash
-  projects
+  puts projects
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
@@ -29,5 +29,3 @@ def create_project_hash
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 end
-
-create_project_hash
